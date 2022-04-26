5c764ecec2a4a482184ab55f149f2fdbe3b9d551
Reese0177 <vinnygo@gmail.com>
2022-04-26 12:54:59 -0500

    Resume and Footer complete

---
+++ components/Footer.js
@@ -0,0 +1,20 @@
+import {SocialIcon} from 'react-social-icons';
+import styles from '../styles/Footer.module.css';
+
+const Footer = ({height}) => {
+    return (
+        <footer style={{height: height}}>
+            <div className={styles.footer}>
+                <div className={styles.icons}>
+                    <SocialIcon className={styles.icon} url="https://www.linkedin.com/in/reese-gosain/" target="_blank"/>
+                    <SocialIcon className={styles.icon} url="https://github.com/Reese0177" target="_blank"/>
+                </div>
+                <div className={styles.text}>
+                    &copy; Reese Gosain
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,27 @@
 import Header from "./Header";
+import Footer from "./Footer";
 import Head from 'next/head';
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-const Layout = React.forwardRef((props, ref) => (
-    <div>
-        <Head>
-            <title>Reese Gosain</title>
-        </Head>
-        <Header ref={ref}/>
-        <div className="container">
-            {props.children}
+const Layout = React.forwardRef((props, ref) => {
+    const [height, setHeight] = useState(0)
+
+    useEffect(() => {
+        setHeight(ref.current.clientHeight)
+    })
+    return (
+        <div>
+            <Head>
+                <title>Reese Gosain</title>
+            </Head>
+            <Header ref={ref} />
+            <div className="container">
+                {props.children}
+            </div>
+            <Footer height={height}/>
         </div>
-    </div>
-));
+    );
+});
 
 export default Layout;
