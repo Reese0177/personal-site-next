23e9995944f086d6acc12dd7a2ff5078cc453379
Reese0177 <vinnygo@gmail.com>
2022-04-27 13:29:08 -0500

    Fixed typos and mobile contact formatting

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Index = (props) => {
             <div className={styles.column + " column"}>
               <h2 className="columnHeader">Past</h2>
               <p>I have had an interest in computers and technology for as long as I can remember. My first related memory is of a toy Superman laptop that I loved as a kid. From there, I started teaching myself Python at age 11, and followed that up with Ruby. Since then, I have also worked with Java, PHP, JS (including React), C++, MySQL, HTML, and CSS.</p>
-              <p>Throughout high school, I continued to develop my skills. I took all of the available computer science classes, was in a technology support internship classes for 3 semesters (during which I helped teach new students as one of the two lead interns), interned with my school district IT department one summer helping prepare for the upcoming school year, and interned at a local building technologies company as an Intern Controls Automation Project Engineer for a semester.</p>
+              <p>Throughout high school, I continued to develop my skills. I took all of the available computer science classes, was in a technology support internship class for 3 semesters (during which I helped teach new students as one of the two lead interns), interned with my school district IT department one summer helping prepare for the upcoming school year, and interned at a local building technologies company as an Intern Controls Automation Project Engineer for a semester.</p>
               <p>After graduation, I decided to take a gap year before college, and moved 500 miles to Nashville, TN (I took the above photo there with my drone.) with my girlfriend. I started studying computer science at Vanderbilt University a year later, and quickly realized it wasn&apos;t for me. After two months, I left and began pursuing apprenticeships.</p>
             </div>
             <div className={styles.column + " column"}>
@@ -36,7 +36,7 @@ const Index = (props) => {
               <h2 className="columnHeader">Future</h2>
               <p>In the future, I hope to lead a small team of people, while also continuing to write code myself. I enjoy the work and can get lost in it at times, and I know that I would never want to give that up to manage people. This role would also allow me to help others learn, which I enjoy doing almost as much as I enjoy coding. I hope to be able to get to this position in a quicker fashion than most, because I believe I have very strong skills and am excited to be able to showcase them in the coming years.</p>
               <p>Ideally, this would be in an environment that will force me to keep learning new skills and technologies, in a way that keeps me motivated. I love being able to see the product of my work, so I hope to work on something consumer-facing.</p>
-              <p>In terms of more personal goals, I would like to move back to a place like Nashville, although Atlanta is on the list, too, and buy my own house. I hope to accomplish that in the much nearer future, hopefully in the next few years.</p>
+              <p>In terms of more personal goals, I would like to move back to a place like Nashville, although Atlanta is on the list, too, and buy my own house. I hope to accomplish that in the very near future, hopefully in the next few years.</p>
             </div>
           </div>
         </div>
