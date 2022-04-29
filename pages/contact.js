c153d5971f3d839d405012a171d3997914a947df
Reese0177 <vinnygo@gmail.com>
2022-04-29 10:09:46 -0500

    Added autofocus hook to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,7 @@ import { Link } from "react-scroll";
 import { useState, useEffect, useRef } from 'react';
 import bg from '../images/Florida.jpg';
 import TextareaAutosize from 'react-textarea-autosize';
+import useAutoFocus from '../hooks/useAutoFocus';
 
 const Index = (props) => {
   const [height, setHeight] = useState(0)
@@ -13,9 +14,11 @@ const Index = (props) => {
     setHeight(header.current.clientHeight)
   }, [])
 
+  const focusedInput = useAutoFocus();
+
   return (
     <Layout ref={header}>
-      <section className="hero" style={{backgroundImage: `url(${bg.src})`}} >
+      <section className="hero" style={{ backgroundImage: `url(${bg.src})` }} >
         <h1 id="title">Let&apos;s Get in Touch</h1>
         <Link to="view" smooth={true} offset={-height} duration={500} className="btn">Do it!</Link>
       </section>
@@ -30,8 +33,8 @@ const Index = (props) => {
           >
             <h2>Contact Me</h2>
             <input type="hidden" name="_subject" value="Contact request from personal site" />
-            <input type="text" name="name" placeholder="Your Name" required/>
-            <input type="email" name="email" placeholder="Your Email" required/>
+            <input type="text" name="name" placeholder="Your Name" ref={focusedInput} required />
+            <input type="email" name="email" placeholder="Your Email" required />
             <TextareaAutosize minRows={10} className={styles.textarea} name="message" placeholder="Your Message" required></TextareaAutosize>
             <button type="submit">Send</button>
           </form>
